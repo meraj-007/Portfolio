272bb98b2ef0dabc6e5d1828537ca4b1c804b5df
meraj-007 <85052647+meraj-007@users.noreply.github.com>
2023-04-21 20:45:24 +0530

    Add files via upload

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiGit
+  SiVercel,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
